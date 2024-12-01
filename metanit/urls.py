2b8d80a81165aed0bf1b23030d9a190d193c7ec9
elvirazakharova <elvirazakharova@yandex.ru>
2024-12-01 14:34:50 +0300

    path и re_path

--- metanit/urls.py
+++ metanit/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from hello import views
-
+ 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    re_path(r'^about', views.about, kwargs={"name":"Tom", "age": 38}),
+    re_path(r'^contact', views.contact),
 ]
